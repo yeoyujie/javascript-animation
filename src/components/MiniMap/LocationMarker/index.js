ee813d069ad5321058f96c5ddd5e37d8ce77f2c8
yeoyujie <yeo.yujie@gmail.com>
2023-06-08 12:05:28 +0800

    Added some simple math to calculate the marker position

--- src/components/MiniMap/LocationMarker/index.js
+++ src/components/MiniMap/LocationMarker/index.js
@@ -1,17 +1,22 @@
+import seedrandom from 'seedrandom'
+
 function LocationMarker({ videoCurrentTime }) {
 
     function calculateCirclePosition(videoCurrentTime) {
-        let x = 0;
-        let y = 0;
-
-        if (videoCurrentTime >= 1 && videoCurrentTime <= 2) {
-            // Move the circle from right to left between 1 and 2 seconds
-            x = 100 - (videoCurrentTime - 1) * 100;
-        } else if (videoCurrentTime >= 3 && videoCurrentTime <= 4) {
-            // Move the circle up between 3 and 4 seconds
-            y = (videoCurrentTime - 3) * 100;
-        }
-
+        // Define the radius of the circle's movement
+        const radius = 50;
+    
+        // Define the duration of one complete revolution in seconds
+        const revolutionDuration = 10;
+    
+        // Calculate the current angle based on the current time of the video
+        // Add an offset of Math.PI / 2 to start from the rightmost point of the circle
+        const angle = (2 * Math.PI * videoCurrentTime) / revolutionDuration + Math.PI / 2;
+    
+        // Calculate the x and y coordinates based on the current angle
+        const x = radius * Math.cos(angle) + radius + 80;
+        const y = radius * Math.sin(angle) + radius + 50;
+    
         return { x, y };
     }
 
